d5c8abfb69837d2ca0bfb137cb763d7a756952d0
kamaudev1 <jk7771313@gmail.com>
2026-07-26 15:15:09 +0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://buyrcwepcwoipbgcydqg.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eXJjd2VwY3dvaXBiZ2N5ZHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNTg1NzcsImV4cCI6MjEwMDYzNDU3N30.g4M6I_2m-3DiD3_WcusLzA67lbNYwugjuf-MEF8UDGg";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
