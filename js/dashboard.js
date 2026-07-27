86f2531f0175b36c603611d56c685ee7315d4036
kamaudev1 <jk7771313@gmail.com>
2026-07-27 11:20:48 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -15,7 +15,21 @@
         return;
     }
 
+    // Welcome message
     document.getElementById("welcome").textContent =
-        `Welcome, ${profile.full_name}!`;
+        `Welcome, ${profile.full_name}`;
+
+    // Show role
+    document.getElementById("role").textContent =
+        profile.role.toUpperCase();
+
+    // Show Admin button only for admins
+    if (profile.role === "admin") {
+
+        document
+            .getElementById("adminMenu")
+            .style.display = "block";
+
+    }
 
 })();
